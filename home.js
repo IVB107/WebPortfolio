0ab341c52aba3f847e2d3f0cc64435726040f999
IVB107 <ianvb107@gmail.com>
2018-05-20 17:20:22 -0500

    Linked jquery, added functionality to home.js

--- home.js
+++ home.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   feather.replace();
+
+  // const changeClass = () => {
+  //   for (let icon of icons){
+  //     icon
+  //   }
+  // };
+  // const icons = document.getElementsByClassName('nav-menu').getElementsByClassName('nav-icon');
+
+  // const icons = document.getElementsByClassName('nav-icon');
+  // icons.onclick() => {
+  //   changeCLass();
+  // }
+
+  $("body").on('click', () => {
+    console.log("Hey");
+  });
+
+  $(".nav-menu").on('click', () => {
+    $(".nav-icon").removeClass("hidden");
+  });
+
 });
