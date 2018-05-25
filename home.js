888a15d5713f76104bdcbe9032dff92c8839883e
IVB107 <ianvb107@gmail.com>
2018-05-24 23:24:39 -0500

    Added functionality to home.js file, modified css for nav menu

--- home.js
+++ home.js
@@ -1,24 +1,50 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   feather.replace();
 
-  // const changeClass = () => {
-  //   for (let icon of icons){
-  //     icon
-  //   }
+  // const home = {
+  //
   // };
-  // const icons = document.getElementsByClassName('nav-menu').getElementsByClassName('nav-icon');
 
-  // const icons = document.getElementsByClassName('nav-icon');
-  // icons.onclick() => {
-  //   changeCLass();
-  // }
+  const animateNav = () => {
+    if ($(".nav-icon").hasClass("hidden")){
+      $("#arrow").css({"transform": "rotate(-180deg)"});
+      $(".nav-icon").not(document.getElementById("arrow")).removeClass("hidden").addClass("visible");
+    } else {
+      $("#arrow").css({"transform": "rotate(0deg)"});
+      $(".nav-icon").not(document.getElementById("arrow")).removeClass("visible").addClass("hidden");
+    }
+  };
 
-  $("body").on('click', () => {
-    console.log("Hey");
+  const subtitles = ["a web developer.", "a student.", "a photographer.", "an audiophile.", "my cat's subordinate human."];
+  var sub;
+
+  const fadeSubtitle = () => {
+    $("#sub").css({"opacity": "0"});
+    setTimeout(()=>{
+      $("#sub").html(subtitles[sub]);
+      $("#sub").css({"opacity": "1"});
+      sub <= 3 ? sub++ : sub = 0;
+      startFade();
+    }, 1500);
+  };
+
+  $("#arrow").on("click", () => {
+    animateNav();
   });
 
-  $(".nav-menu").on('click', () => {
-    $(".nav-icon").removeClass("hidden");
+  $(".subtitles").mouseover(()=>{
+    if (sub){
+      return void 0;
+    } else {
+      sub = 1;
+      fadeSubtitle();
+    }
   });
+
+  startFade = ()=>{
+    setTimeout(()=>{
+      fadeSubtitle();
+    }, 3000);
+  };
 
 });
